9203b6e3d8fa42dba0a3bda7743ece72298298bc
oryon-dominik <oryon@cyberise.de>
2021-12-01 09:13:43 +0100

    Day 1 2021

---
+++ 2021/01.py
@@ -0,0 +1,59 @@
+import logging
+from puzzle_handler import AdventPuzzleHandler
+
+
+log = logging.getLogger('Advent of Code Day 1')
+
+
+"""These are dirty and hacky solutions but they work.. have to think it through more elegantly..."""
+
+
+class SonarSweep(AdventPuzzleHandler):
+
+    approaches = {
+            'previous': {"func": "compare_to_previous", "datatype": list},
+            'window': {"func": "compare_window", "datatype": list},
+        }
+    puzzle_day = 1
+    clean_data = True
+
+    def clean_data(self, data):
+        return [int(d) for d in data]
+
+    def compare_to_previous(self):
+        """How many measurements are larger than the previous measurement?"""
+        increasing = 0
+        for measurement, depth in enumerate(self.data):
+            try:
+                if self.data[measurement] > self.data[measurement - 1]:
+                    log.debug(f"depth={depth} previous={self.data[measurement - 1]} increasing={depth - self.data[measurement - 1]}")
+                    increasing += 1
+            except IndexError:
+                pass
+        return increasing
+
+    def compare_window(self, window_depth=3):
+        """How many sums are larger than the previous sum?"""
+        wd = window_depth
+        increasing = 0
+        for measurement, depth in enumerate(self.data):
+            m = measurement
+            n = m + 1
+            try:
+                this_ = sum(self.data[m : m + wd])
+                next_ = sum(self.data[n: n + wd])
+                log.debug(f"depth={depth} this_window={this_} next_window={next_} increasing={next_ - this_}")
+                if this_ < next_:
+                    increasing += 1
+            except IndexError:
+                pass
+        return increasing
+
+
+logging.basicConfig(level=logging.INFO, format='{levelname}: {message}', style='{')
+
+ssweep = SonarSweep(approach='previous', timeit=True)
+log.info(f"{ssweep.text} | Task1 - Compare to previous: {ssweep.result} - {ssweep.time}")
+
+ssweep = SonarSweep(approach='window', timeit=True)
+log.info(f"{ssweep.text} | Task2 - Compare window: {ssweep.result} - {ssweep.time}")
