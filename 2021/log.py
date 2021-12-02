eae54fe986482210d56d5c7a7122e47a367c6bde
oryon-dominik <oryon@cyberise.de>
2021-12-02 09:01:28 +0100

    rich logging

--- 2021/log.py
+++ 2021/log.py
@@ -1,3 +1,14 @@
 import logging
-logging.basicConfig(level=logging.INFO, format='{levelname}: {message}', style='{')
+from rich.logging import RichHandler
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='{levelname}: {message}',
+    style='{', handlers=[RichHandler(
+        rich_tracebacks = True,
+        show_time = False,
+    )]
+)
+
 log = logging.getLogger('Advent of Code')
