2f31ae6d37e496f1121fe485b74b42eef0af73fb
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2022-12-01 09:38:12 +0100

    Day1

--- 2022/01.py
+++ 2022/01.py
@@ -5,12 +5,22 @@ from logs import log
 """These are dirty and hacky solutions but they work.. have to think it through more elegantly..."""
 
 
-class Advent(Puzzle):
+class CalorieCountingAdvent(Puzzle):
     puzzle_day = 1
+    clean_data = True
 
-    def advent(self):
-        data: list[int] = [int(d) for d in self.data]
-        return sum(data)
+    def clean(self, data: str):
+        return data.split('\n\n')
 
-done = Advent(approach='advent', day=1)
+    def total_calories(self):
+        return max([sum(int(e) for e in elf.split('\n')) for elf in self.data])
+
+    def top_three_total_calories(self):
+        return sum(sorted([sum(int(e) for e in elf.split('\n')) for elf in self.data])[-3:])
+
+
+done = CalorieCountingAdvent(approach='total_calories', day=1, read_file_as='raw')
+log.info(f"{done.text} | {done.approach.capitalize()}: {done.result} - {done.time}")
+
+done = CalorieCountingAdvent(approach='top_three_total_calories', day=1, read_file_as='raw')
 log.info(f"{done.text} | {done.approach.capitalize()}: {done.result} - {done.time}")
